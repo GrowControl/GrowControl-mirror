51463c3116969ad12c59afe6b080c94cda5ce100
Pete Lorenzo <lorenzo@poixson.com>
2014-05-15 03:28:22 -0400

    changed gcServerVars to extend xVars and xAppVars classes

--- server/src/com/growcontrol/server/gcServerVars.java
+++ server/src/com/growcontrol/server/gcServerVars.java
@@ -1,53 +1,74 @@
 package com.growcontrol.server;
 
+import com.poixson.commonapp.xAppVars;
 import com.poixson.commonjava.EventListener.xHandler;
 
 
-public final class ServerListeners {
+public final class gcServerVars extends xAppVars {
+
+
+	// singleton instance
+	private static volatile gcServerVars instance = null;
+	public static gcServerVars get() {
+		if(instance == null) {
+			synchronized(instanceLock) {
+				if(instance == null)
+					instance = new gcServerVars();
+			}
+		}
+		return instance;
+	}
 	@Override
 	public Object clone() throws CloneNotSupportedException {
 		throw new CloneNotSupportedException();
 	}
 
 
-	// single instance of holder class
-	private static volatile ServerListeners instance = null;
-	private static final Object lock = new Object();
-	public static ServerListeners get() {
-		if(instance == null) {
-			synchronized(lock) {
-				if(instance == null)
-					instance = new ServerListeners();
-			}
-		}
-		return instance;
-	}
-
-
-	private final xHandler system;
-	private final xHandler commands;
-	private final xHandler plugins;
-
-
 	// new instance of holder
-	private ServerListeners() {
-		this.system   = new xHandler();
-		this.commands = new xHandler();
-		this.plugins  = new xHandler();
+	protected gcServerVars() {
+		super();
+		// clone vars
+		if(instance != null) {
+System.out.println("CLONING gcServerVars");
+			this.system   = instance.system;
+			this.commands = instance.commands;
+			this.plugins  = instance.plugins;
+			this.router   = instance.router;
+		// new instance
+		} else {
+			this.system   = new xHandler();
+			this.commands = new xHandler();
+			this.plugins  = new xHandler();
+			this.router   = new xHandler();
+		}
 	}
 
 
 	// system event handler
+	private final xHandler system;
 	public xHandler system() {
 		return this.system;
 	}
+
+
 	// command event handler
+	private final xHandler commands;
 	public xHandler commands() {
 		return this.commands;
 	}
+
+
 	// plugin event handler
+	private final xHandler plugins;
 	public xHandler plugins() {
 		return this.plugins;
+	}
+
+
+	// meta router handler
+	private final xHandler router;
+	public xHandler router() {
+		return this.router;
 	}
 
 
