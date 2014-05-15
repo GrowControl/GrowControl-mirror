1c90038850b7059af31f81a32870c2f78a9fc679
Pete Lorenzo <lorenzo@poixson.com>
2014-05-15 13:47:59 -0400

    added gcClientVars which extends xVars and xAppVars

---
+++ client/src/com/growcontrol/client/gcClientVars.java
@@ -0,0 +1,74 @@
+package com.growcontrol.client;
+
+import com.poixson.commonapp.xAppVars;
+
+
+public class gcClientVars extends xAppVars {
+
+
+	// singleton instance
+	private static volatile gcClientVars instance = null;
+	public static gcClientVars get() {
+		if(instance == null) {
+			synchronized(instanceLock) {
+				if(instance == null)
+					instance = new gcClientVars();
+			}
+		}
+		return instance;
+	}
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		throw new CloneNotSupportedException();
+	}
+
+
+	// new instance of holder
+	protected gcClientVars() {
+		super();
+		// clone vars
+		if(instance != null) {
+System.out.println("CLONING gcClientVars");
+//			this.system   = instance.system;
+//			this.commands = instance.commands;
+//			this.plugins  = instance.plugins;
+//			this.router   = instance.router;
+		// new instance
+		} else {
+//			this.system   = new xHandler();
+//			this.commands = new xHandler();
+//			this.plugins  = new xHandler();
+//			this.router   = new xHandler();
+		}
+	}
+
+
+//	// system event handler
+//	private final xHandler system;
+//	public xHandler system() {
+//		return this.system;
+//	}
+
+
+//	// command event handler
+//	private final xHandler commands;
+//	public xHandler commands() {
+//		return this.commands;
+//	}
+
+
+//	// plugin event handler
+//	private final xHandler plugins;
+//	public xHandler plugins() {
+//		return this.plugins;
+//	}
+
+
+//	// meta router handler
+//	private final xHandler router;
+//	public xHandler router() {
+//		return this.router;
+//	}
+
+
+}
