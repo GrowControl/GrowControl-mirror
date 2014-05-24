c876d060fff8c94ea61b4472401a7fd9913f6c81
Pete Lorenzo <lorenzo@poixson.com>
2014-05-24 13:24:45 -0400

    added gcServerPlugin and gcClientPlugin classes to gccommon package

--- gccommon/plugins/gcClientPlugin.java
+++ gccommon/plugins/gcClientPlugin.java
@@ -1,7 +1,16 @@
 package com.growcontrol.gccommon.plugins;
 
+import com.poixson.commonapp.plugin.xJavaPlugin;
+import com.poixson.commonapp.plugin.xPluginManager;
+import com.poixson.commonapp.plugin.xPluginYML;
 
-public abstract class xClientPlugin extends xJavaPlugin {
+
+public abstract class gcClientPlugin extends xJavaPlugin {
+
+
+	public gcClientPlugin(final xPluginManager manager, final xPluginYML yml) {
+		super(manager, yml);
+	}
 
 
 }
--- gccommon/plugins/gcServerPlugin.java
+++ gccommon/plugins/gcServerPlugin.java
@@ -1,7 +1,16 @@
 package com.growcontrol.gccommon.plugins;
 
+import com.poixson.commonapp.plugin.xJavaPlugin;
+import com.poixson.commonapp.plugin.xPluginManager;
+import com.poixson.commonapp.plugin.xPluginYML;
 
-public abstract class xServerPlugin extends xJavaPlugin {
+
+public abstract class gcServerPlugin extends xJavaPlugin {
+
+
+	public gcServerPlugin(final xPluginManager manager, final xPluginYML yml) {
+		super(manager, yml);
+	}
 
 
 }
