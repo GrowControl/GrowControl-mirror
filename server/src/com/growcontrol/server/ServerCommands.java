15f7fd97e57cb744ed18c8aca4146324c69577a5
Pete Lorenzo <lorenzo@poixson.com>
2014-05-25 19:34:37 -0400

    removed old comments from ServerCommands class

--- server/src/com/growcontrol/server/ServerCommands.java
+++ server/src/com/growcontrol/server/ServerCommands.java
@@ -179,24 +179,6 @@ event.setHandled();
 		final xLog log = xLog.getRoot();
 		log.publish("Current log level: "+log.getLevel().toString());
 	}
-//	// help command
-//	private static boolean _route(pxnCommandEvent commandEvent) {
-//		String[] args = commandEvent.getArgs();
-//		if(args.length != 3) {
-//			pxnLog.get().Publish("Invalid arguments!\n"+commandEvent.command.getUsageStr());
-//			return true;
-//		}
-//		String toName = args[0];
-//		metaValue meta = metaValue.newValue(args[1], args[2]);
-//		if(meta == null) {
-//			pxnLog.get().Publish("Invalid meta type!");
-//TODO: list meta types
-//			return true;
-//		}
-//		// send to router
-//		meta.Send(toName);
-//		return true;
-//	}
 
 
 	@Override
@@ -205,137 +187,16 @@ event.setHandled();
 	}
 
 
-//	@Override
-//	public boolean onCommand(pxnCommandEvent commandEvent) {
-//		if(commandEvent.isHandled()) return false;
-//		switch(commandEvent.getCommandStr()) {
-//		// server control
-//		case "stop":
-//			return ServerCommands._stop();
-//		case "kill":
-//			return _kill();
-//		case "start":
-//			return _start();
-//		case "pause":
-//			return _pause();
-//		case "clear":
-//			return _clear();
-//		case "help":
-//			return _help(commandEvent.getArgs());
-//		case "log":
-//			return _log(commandEvent.getArgs());
-//		case "show":
-//			return _show(commandEvent.getArgs());
-//		case "version":
-//			return _version();
-//		case "say":
-//			String msg = commandEvent.raw;
-//			if(msg.contains(" ")) msg = msg.substring(msg.indexOf(" ") + 1);
-//			return _say(msg);
-//		// tools
-//		case "ping":
-//			return _ping( (String[]) event.args.toArray());
-//		case "threads":
-//			return _threads();
-//		case "route":
-//			return _route(commandEvent);
-//		default:
-//			break;
-//		}
-//		return false;
-//	}
-
-
-//	// stop command
-//	private static boolean _stop() {
-//		gcServer.get().Shutdown();
-//		return true;
-//	}
-
-
-//	// kill command
-//	private static boolean _kill() {
-//		try {
-//			pxnLog.get().warning("Killing server! (Triggered by console command)");
-//		} catch(Exception ignore) {}
-//		System.out.println();
-//		System.out.println();
-//		System.exit(0);
-//		return true;
-//	}
-
-
-//	// start command
-//	private static boolean _start() {
-//		return true;
-//	}
-//	// pause command
-//	private static boolean _pause() {
-//		return true;
-//	}
-
-
-//	// help command
-//	private static boolean _help(String[] args) {
-//		return true;
-//	}
-
-
-//	// log command
-//	private static boolean _log(String[] args) {
-//		pxnLevel level = null;
-//		// 0 args
-//		if(args.length == 0) {
-//			_log();
-//			return true;
-//		} else
-//		// 1 arg
-//		if(args.length == 1) {
-//			if(args[0].equalsIgnoreCase("level")) {
-//				_log();
-//				return true;
-//			}
-//			level = pxnLevel.Parse(args[0]);
-//		} else
-//		// 2 args
-//		if(args.length == 2 && args[0].equalsIgnoreCase("level")) {
-//			level = pxnLevel.Parse(args[1]);
-//		}
-//		// invalid
-//		if(level == null)
-//			return false;
-//		gcServer.get().setLogLevel(level);
-//		return true;
-//	}
-//	// show command
-//	private static boolean _show(String[] args) {
-//		return true;
-//	}
-
-
-//	// version command
-//	private static boolean _version() {
-//		pxnLog.get().info("Version: "+gcServer.version);
-//		return true;
-//	}
-
-
-//	// say command
 //	private static boolean _say(String msg) {
 //		msg = "(console) "+msg;
 //		pxnLog.get().Publish(msg);
 //		return true;
 //	}
-
-
-//	// ping command
-//	private static boolean _ping(String[] args) {
-//		return true;
-//	}
-
-
-//	// threads command
-//	private static boolean _threads() {
+//	// say
+//	if(command.equals("say")) {
+//		String msg = "";
+//		for(String line : args) msg += " "+line;
+//		gcServer.log.info("Server says:"+msg);
 //		return true;
 //	}
 
@@ -358,30 +219,6 @@ event.setHandled();
 //			gcServer.log.info("[ Tools ]");
 //			gcServer.log.info("ping - ");
 //			gcServer.log.info("threads - ");
-// input / output
-//set
-//get
-//watch
-//stop
-//exit
-//quit
-//shutdown
-//kill
-//			return true;
-//		}
-//		// version
-//		if(command.equals("version")) {
-//			//TODO:
-//			gcServer.log.info("GrowControl "+gcServer.version);
-//			return true;
-//		}
-//		// say
-//		if(command.equals("say")) {
-//			String msg = "";
-//			for(String line : args) msg += " "+line;
-//			gcServer.log.info("Server says:"+msg);
-//			return true;
-//		}
 
 		// list plugins/devices/inputs/outputs
 //		if(command.equals("list")) {
@@ -400,22 +237,6 @@ event.setHandled();
 //					return true;
 //				}
 //			}
-//			gcServer.log.info("Usage: "+command.getUsage());
-//			return true;
-//		}
-//		if(command.equals("plugins")) {
-//			//TODO:
-//			gcServer.log.warning("command not yet implemented");
-////			gcPluginLoader.listPlugins();
-//			return true;
-//		}
-//		// list devices
-//		if(command.equals("devices")) {
-//			//TODO:
-//			gcServer.log.warning("command not yet implemented");
-////			gcServer.deviceLoader.listDevices();
-//			return true;
-//		}
 //
 //		// set input / output
 //		if(command.equals("set")) {
@@ -428,36 +249,6 @@ event.setHandled();
 ////			gcServer.log.warning("Failed to find an output plugin! "+msg);
 //			return true;
 //		}
-//		// get input / output
-//		if(command.equals("get")) {
-//			//TODO:
-//			gcServer.log.warning("command not yet implemented");
-//			return true;
-//		}
-//		// watch input / output
-//		if(command.equals("watch")) {
-//			//TODO:
-//			gcServer.log.warning("command not yet implemented");
-//			return true;
-//		}
-//
-//		// ping
-//		if(command.equals("ping")) {
-//			//TODO:
-//			gcServer.log.warning("command not yet implemented");
-//			return true;
-//		}
-//
-//		return false;
-//	}
-
-
-//	private static void listDevices() {
-//	}
-//	private static void listOutputs() {
-//	}
-//	private static void listInputs() {
-//	}
 
 
 //	if(command.equalsIgnoreCase("threads")) {
