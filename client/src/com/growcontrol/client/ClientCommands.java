3aac0f831d5b2d7c9c17644063b390c3fa228289
Pete Lorenzo <lorenzo@poixson.com>
2014-05-25 21:19:58 -0400

    started adding commands to ClientCommands class

--- client/src/com/growcontrol/client/ClientCommands.java
+++ client/src/com/growcontrol/client/ClientCommands.java
@@ -1,38 +1,87 @@
 package com.growcontrol.client;
 
-import com.growcontrol.gcCommon.pxnCommand.pxnCommandEvent;
-import com.growcontrol.gcCommon.pxnCommand.pxnCommandsHolder;
+import com.poixson.commonapp.app.xApp;
+import com.poixson.commonapp.listeners.CommandEvent;
+import com.poixson.commonjava.EventListener.xEvent;
+import com.poixson.commonjava.EventListener.xEvent.Priority;
+import com.poixson.commonjava.EventListener.xListener;
+import com.poixson.commonjava.xLogger.xLog;
 
 
-public final class ClientCommands extends pxnCommandsHolder {
-	private ClientCommands() {}
+public final class ClientCommands implements xListener {
+
+
+	@xEvent(
+		priority=Priority.NORMAL,
+		filtered=false,
+		threaded=false,
+		ignoreHandled=true,
+		ignoreCancelled=true)
+	public void onCommand(final CommandEvent event) {
+		switch(event.arg(0)) {
+		// shutdown
+		case "shutdown":
+		case "stop":
+		case "exit":
+		case "quit":
+			_shutdown(event);
+			break;
+		// kill
+		case "kill":
+		case "end":
+			_kill(event);
+			System.exit(0);
+			break;
+		// clear
+		case "clear":
+		case "cls":
+			_clear(event);
+			break;
+		case "set":
+			_set(event);
+			break;
+		case "test":
+			_test(event);
+			break;
+		}
+	}
+
+
+	private static void _test(final CommandEvent event) {
+		event.setHandled();
+	}
+
+
+	// shutdown command
+	private static void _shutdown(final CommandEvent event) {
+		event.setHandled();
+		xApp.get().shutdown();
+	}
+	// kill command
+	private static void _kill(final CommandEvent event) {
+		event.setHandled();
+		System.out.println("Killed by command.");
+		System.out.println();
+		System.exit(0);
+	}
+
+
+	// clear command
+	private static void _clear(final CommandEvent event) {
+		event.setHandled();
+		xLog.getConsole().clear();
+	}
+
+
+	// trigger event manually
+	private void _set(final CommandEvent event) {
+		event.setHandled();
+	}
+
+
 	@Override
 	public Object clone() throws CloneNotSupportedException {
 		throw new CloneNotSupportedException();
-	}
-
-	private static volatile ClientCommands instance = null;
-	private static final Object lock = new Object();
-
-
-	public static ClientCommands get() {
-		if(instance == null) {
-			synchronized(lock) {
-				if(instance == null)
-					instance = new ClientCommands();
-			}
-		}
-		return instance;
-	}
-	@Override
-	protected void initCommands() {
-//TODO:
-	}
-
-
-	@Override
-	protected boolean onCommand(pxnCommandEvent command) {
-		return false;
 	}
 
 
