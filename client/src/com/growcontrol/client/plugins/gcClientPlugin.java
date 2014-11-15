1d86f79cc2c89e0a4110aa783de3fdaa91cd549d
Lop <lop@matt.lophome.com>
2014-11-14 19:46:19 -0500

    added register() to gcServerPlugin and gcClientPlugin abstract classes

--- client/src/com/growcontrol/client/plugins/gcClientPlugin.java
+++ client/src/com/growcontrol/client/plugins/gcClientPlugin.java
@@ -1,10 +1,28 @@
 package com.growcontrol.client.plugins;
 
+import com.growcontrol.client.gcClientVars;
 import com.poixson.commonapp.plugin.xJavaPlugin;
+import com.poixson.commonjava.EventListener.xListener;
+import com.poixson.commonjava.xLogger.handlers.xCommandListener;
 
 
 public abstract class gcClientPlugin extends xJavaPlugin {
 
+
+
+	@Override
+	public void register(final xListener listener) {
+		if(listener == null) throw new NullPointerException("Cannot register null listener");
+		final gcClientVars vars = gcClientVars.get();
+		// command listener
+		if(listener instanceof xCommandListener) {
+			vars.commands().register(listener);
+		// unknown
+		} else {
+			throw new RuntimeException("Cannot register unknown listener type: "
+					+listener.getClass().getName());
+		}
+	}
 
 
 
--- server/src/com/growcontrol/server/plugins/gcServerPlugin.java
+++ server/src/com/growcontrol/server/plugins/gcServerPlugin.java
@@ -1,10 +1,28 @@
 package com.growcontrol.server.plugins;
 
+import com.growcontrol.server.gcServerVars;
 import com.poixson.commonapp.plugin.xJavaPlugin;
+import com.poixson.commonjava.EventListener.xListener;
+import com.poixson.commonjava.xLogger.handlers.xCommandListener;
 
 
 public abstract class gcServerPlugin extends xJavaPlugin {
 
+
+
+	@Override
+	public void register(final xListener listener) {
+		if(listener == null) throw new NullPointerException("Cannot register null listener");
+		final gcServerVars vars = gcServerVars.get();
+		// command listener
+		if(listener instanceof xCommandListener) {
+			vars.commands().register(listener);
+		// unknown
+		} else {
+			throw new RuntimeException("Cannot register unknown listener type: "
+					+listener.getClass().getName());
+		}
+	}
 
 
 
