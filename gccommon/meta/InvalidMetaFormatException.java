bd14f4c18ca040f8dae1ac8373a7a3c893f998fa
Lop <lop@matt.lophome.com>
2014-11-26 19:19:02 -0500

    added InvalidMetaFormatException

---
+++ gccommon/meta/InvalidMetaFormatException.java
@@ -0,0 +1,18 @@
+package com.growcontrol.gccommon.meta;
+
+
+public class InvalidMetaFormatException extends NumberFormatException {
+	private static final long serialVersionUID = 31L;
+
+
+
+	public InvalidMetaFormatException() {
+		super();
+	}
+	public InvalidMetaFormatException(final String msg) {
+		super(msg);
+	}
+
+
+
+}
