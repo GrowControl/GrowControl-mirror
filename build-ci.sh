66b77d8f8227f9dbfef9263dd04f4fba464d5b12
lorenzop <lorenzo@poixson.com>
2014-12-10 08:18:06 -0500

    updated build-ci.sh script

--- build-ci.sh
+++ build-ci.sh
@@ -1,31 +1,51 @@
-# (cd ${WORKSPACE}/GrowControl.git/; sh build-ci.sh)
+# sh GrowControl.git/build-ci.sh  --dl-path=/home/pxn/www/dl/GrowControl  --yum-path=/home/pxn/www/yum/extras-testing/noarch
 
 
+# load build_utils.sh script
+if [ -e build_utils.sh ]; then
+	source ./build_utils.sh
+elif [ -e /usr/local/bin/pxn/build_utils.sh ]; then
+	source /usr/local/bin/pxn/build_utils.sh
+else
+	wget https://raw.githubusercontent.com/PoiXson/shellscripts/master/pxn/build_utils.sh \
+		|| exit 1
+	source ./build_utils.sh
+fi
 
-echo "Checkout.."
-( cp -fv "${WORKSPACE}/GrowControl.git/setup_workspace.sh" "${WORKSPACE}/setup_workspace.sh" ) || exit 1
+
+NAME="GrowControl"
+NAMEs="gcServer"
+NAMEc="gcClient"
+[ -z "${WORKSPACE}" ] && WORKSPACE=`pwd`
+rm -vf "${WORKSPACE}/${NAME}.git/${NAMEs}"-*.jar
+rm -vf "${WORKSPACE}/${NAME}.git/${NAMEc}"-*.jar
+rm -vf "${WORKSPACE}/${NAME}.git/${NAMEs}"-*.noarch.rpm
+rm -vf "${WORKSPACE}/${NAME}.git/${NAMEc}"-*.noarch.rpm
+rm -vf "${WORKSPACE}/${NAME}.git/${NAME}"-*.zip
+
+
+title "Checkout.."
+( cp -fv "${WORKSPACE}/${NAME}.git/setup_workspace.sh" "${WORKSPACE}/setup_workspace.sh" ) || exit 1
 ( cd "${WORKSPACE}" && sh setup_workspace.sh --https --user ) || exit 1
 
 
-
-echo "Pre-build Cleanup.."
-rm -fv "${WORKSPACE}/GrowControl-"*.zip
-rm -fv "${WORKSPACE}/GrowControl.git/GrowControl-"*.zip
-rm -fv "${WORKSPACE}/gc"{Server,Client}-*.{jar,noarch.rpm}
-rm -fv "${WORKSPACE}/GrowControl.git/gc"{Server,Client}-*.{jar,noarch.rpm}
+title "Build.."
+( cd "${WORKSPACE}/${NAME}.git/" && sh build-mvn.sh --build-number ${BUILD_NUMBER} ) || exit 1
+( cd "${WORKSPACE}/${NAME}.git/" && sh build-rpm.sh --build-number ${BUILD_NUMBER} ) || exit 1
 
 
-
-echo "Build.."
-( cd "${WORKSPACE}/GrowControl.git/" && sh build-mvn.sh --build-number ${BUILD_NUMBER} ) || exit 1
-( cd "${WORKSPACE}/GrowControl.git/" && sh build-rpm.sh --build-number ${BUILD_NUMBER} ) || exit 1
-( cp -fv "${WORKSPACE}/GrowControl.git/gc"{Server,Client}-*.{jar,noarch.rpm} "${WORKSPACE}" ) || exit 1
-( cp -fv "${WORKSPACE}/GrowControl.git/GrowControl-"*.zip                    "${WORKSPACE}" ) || exit 1
-
-
-
-echo "Deploy.."
-rm -fv "/home/pxn/www/yum/extras-testing/noarch/gc"{Server,Client}-*.noarch.rpm
-cp -fv "${WORKSPACE}/gc"{Server,Client}-*.noarch.rpm "/home/pxn/www/yum/extras-testing/noarch/" || exit 1
-
+title "Deploy.."
+cp -fv "${WORKSPACE}/${NAME}.git/${NAMEs}"-*.jar        "${DL_PATH}/" || exit 1
+cp -fv "${WORKSPACE}/${NAME}.git/${NAMEc}"-*.jar        "${DL_PATH}/" || exit 1
+cp -fv "${WORKSPACE}/${NAME}.git/${NAMEs}"-*.noarch.rpm "${DL_PATH}/" || exit 1
+cp -fv "${WORKSPACE}/${NAME}.git/${NAMEc}"-*.noarch.rpm "${DL_PATH}/" || exit 1
+cp -fv "${WORKSPACE}/${NAME}.git/${NAME}"-*.zip         "${DL_PATH}/" || exit 1
+# server
+latest_version "${DL_PATH}/${NAMEs}-*.noarch.rpm"         || exit 1
+echo "Latest version: "${LATEST_FILE}
+ln -fs "${LATEST_FILE}" "${YUM_PATH}/${NAMEs}.noarch.rpm" || exit 1
+# client
+latest_version "${DL_PATH}/${NAMEc}-*.noarch.rpm"         || exit 1
+echo "Latest version: "${LATEST_FILE}
+ln -fs "${LATEST_FILE}" "${YUM_PATH}/${NAMEc}.noarch.rpm" || exit 1
 
