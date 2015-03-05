bb1d2435e7c603fb818d88a95bfc496a03a2fa10
lorenzop <lorenzo@poixson.com>
2015-03-05 00:08:25 -0500

    added netty hello world web server code to NetManager class added temporary initializer and handler classes to NetManager class added Start() and Stop() function code and added log() function

--- server/src/com/growcontrol/server/net/NetManager.java
+++ server/src/com/growcontrol/server/net/NetManager.java
@@ -1,21 +1,56 @@
 package com.growcontrol.server.net;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.ssl.SslContext;
+
+import java.net.UnknownHostException;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import com.growcontrol.server.configs.gcSocketDAO;
 import com.poixson.commonjava.Utils.Keeper;
 import com.poixson.commonjava.Utils.utils;
 import com.poixson.commonjava.Utils.xStartable;
+import com.poixson.commonjava.Utils.threads.xThreadFactory;
+import com.poixson.commonjava.xLogger.xLog;
 
 
 public class NetManager implements xStartable {
 
+	public static final int BACKLOG = 10;
+
 	private static volatile NetManager instance = null;
 	private static final Object instanceLock = new Object();
 
+	protected final AtomicBoolean running = new AtomicBoolean(false);
+	protected final Set<NetServer> servers = new HashSet<NetServer>();
 	protected final Set<gcSocketDAO> configs = new CopyOnWriteArraySet<gcSocketDAO>();
+
+	protected final ServerBootstrap bootstrap;
+	protected final EventLoopGroup bossGroup;
+	protected final EventLoopGroup workGroup;
 
 
 
@@ -33,6 +68,112 @@ public class NetManager implements xStartable {
 
 	public NetManager() {
 		Keeper.add(this);
+		// thread groups
+		final int cores = Runtime.getRuntime().availableProcessors();
+		this.bossGroup = new NioEventLoopGroup(1,     new xThreadFactory("netty-boss", true));
+		this.workGroup = new NioEventLoopGroup(cores, new xThreadFactory("netty-work", true));
+		// server bootstrap
+		this.bootstrap = new ServerBootstrap();
+		this.bootstrap.group(this.bossGroup, this.workGroup);
+		this.bootstrap.option(ChannelOption.SO_BACKLOG, BACKLOG);
+		this.bootstrap.channel(NioServerSocketChannel.class);
+		// handlers
+//		this.bootstrap.handler(new LoggingHandler(LogLevel.INFO));
+		final SslContext sslContext = null;
+		this.bootstrap.childHandler(
+				new ServerSocketChannelInitializer(sslContext)
+		);
+//		// this method is bad for servers
+//		this.bossGroup = Executors.newCachedThreadPool();
+//		this.workGroup = Executors.newCachedThreadPool();
+//		// this is used in netty examples
+//		this.bossGroup = new NioEventLoopGroup();
+//		this.workGroup = new NioEventLoopGroup();
+//		this.childHandler = new ChannelInitializer<SocketChannel>() {
+//			@Override
+//			protected void initChannel(SocketChannel ch) throws Exception {
+//			}
+//		}
+	}
+
+
+
+	protected static class ServerSocketChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+		private final SslContext sslContext;
+
+		public ServerSocketChannelInitializer(final SslContext sslContext) {
+			this.sslContext = sslContext;
+		}
+
+		@Override
+		protected void initChannel(final SocketChannel ch) throws Exception {
+			final ChannelPipeline pipe = ch.pipeline();
+			if(this.sslContext != null)
+				pipe.addLast(this.sslContext.newHandler(ch.alloc()));
+			pipe.addLast(new HttpServerCodec());
+			pipe.addLast(
+					new ServerSocketChannelHandler()
+			);
+		}
+
+	}
+
+
+
+	protected static class ServerSocketChannelHandler extends ChannelInboundHandlerAdapter {
+
+		private static final byte[] CONTENT = "Hello World!!!".getBytes();
+
+		@Override
+		public void channelReadComplete(final ChannelHandlerContext context) {
+			context.flush();
+		}
+
+		@Override
+		public void channelRead(final ChannelHandlerContext context, final Object msg) {
+			if(msg instanceof HttpRequest) {
+				final HttpRequest request = (HttpRequest) msg;
+				if(HttpHeaders.is100ContinueExpected(request)) {
+					context.write(
+						new DefaultFullHttpResponse(
+							HttpVersion.HTTP_1_1,
+							HttpResponseStatus.CONTINUE
+						)
+					);
+				}
+				final boolean keepalive = HttpHeaders.isKeepAlive(request);
+				final FullHttpResponse response = new DefaultFullHttpResponse(
+						HttpVersion.HTTP_1_1,
+						HttpResponseStatus.OK,
+						Unpooled.wrappedBuffer(CONTENT)
+				);
+				response.headers().set(
+						HttpHeaders.Names.CONTENT_TYPE,
+						"text/plain"
+				);
+				response.headers().set(
+						HttpHeaders.Names.CONTENT_LENGTH,
+						response.content().readableBytes()
+				);
+				if(!keepalive) {
+					context.write(response).addListener(ChannelFutureListener.CLOSE);
+				} else {
+					response.headers().set(
+							HttpHeaders.Names.CONNECTION,
+							HttpHeaders.Values.KEEP_ALIVE
+					);
+					context.write(response);
+				}
+			}
+		}
+
+		@Override
+		public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+			cause.printStackTrace();
+			ctx.close();
+		}
+
 	}
 
 
@@ -48,19 +189,93 @@ public class NetManager implements xStartable {
 
 	@Override
 	public void Start() {
+		// ensure stopped
+		if(this.running.get())
+			this.Stop();
+		// get configs
+		final Set<gcSocketDAO> cfgs = new HashSet<gcSocketDAO>(this.configs);
+		if(cfgs.isEmpty()) {
+			this.log().fine("No socket server configs loaded");
+			return;
+		}
+		// only start once
+		if(!this.running.compareAndSet(false, true))
+			return;
+
+
+		// start socket servers
+		synchronized(this.servers) {
+			for(final gcSocketDAO cfg : cfgs) {
+				if(!cfg.enabled) continue;
+				try {
+					final NetServer server = new NetServer(this, cfg);
+					this.servers.add(server);
+				} catch (UnknownHostException e) {
+					this.log().severe("Failed to start socket server "+cfg.toString());
+					this.log().trace(e);
+				} catch (InterruptedException e) {
+					this.log().trace(e);
+					this.Stop();
+					return;
+				}
+			}
+		}
+
+
+
+
+		// unexpected shutdown
+		if(!this.running.get())
+			this.Stop();
 	}
 	@Override
 	public void Stop() {
+		synchronized(this.servers) {
+			this.running.set(false);
+			final Iterator<NetServer> it = this.servers.iterator();
+			while(it.hasNext()) {
+				it.next().close();
+				it.remove();
+			}
+			this.servers.clear();
+		}
+	}
+	public void CloseAll() {
+//		synchronized(this.channels) {
+//			final Iterator<Channel> it = this.channels.iterator();
+//			while(it.hasNext()) {
+//				it.next().close().sync();
+//				it.remove();
+//			}
+//			this.channels.clear();
+//		}
 	}
 
 
 
 	@Override
 	public void run() {
+		throw new UnsupportedOperationException();
 	}
 	@Override
 	public boolean isRunning() {
-		return false;
+		return this.running.get();
+	}
+
+
+
+	// logger
+	private volatile xLog _log = null;
+	private volatile xLog _log_default = null;
+	public xLog log() {
+		if(this._log != null)
+			return this._log;
+		if(this._log_default == null)
+			this._log_default = xLog.getRoot().get("netty");
+		return this._log_default;
+	}
+	public void setLog(final xLog log) {
+		this._log = log;
 	}
 
 
