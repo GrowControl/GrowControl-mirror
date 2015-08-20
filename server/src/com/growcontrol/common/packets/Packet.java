d571f5fda6bb85cfdbe3bb1ad13d14753e825d3b
lorenzop <lorenzo@poixson.com>
2015-08-20 05:16:26 -0400

    cleaned up and fixed convert() functions in Packet class also changed them to static

--- server/src/com/growcontrol/common/packets/Packet.java
+++ server/src/com/growcontrol/common/packets/Packet.java
@@ -5,7 +5,9 @@ import java.util.Map;
 
 import org.yaml.snakeyaml.Yaml;
 
+import com.poixson.commonjava.Utils.utils;
 import com.poixson.commonjava.Utils.utilsObject;
+import com.poixson.commonjava.xLogger.xLog;
 
 
 public abstract class Packet {
@@ -25,23 +27,40 @@ public abstract class Packet {
 
 
 
-	public String convert(final Object json) {
-		return this.getYaml()
-				.dump(json);
+	public static String convert(final Yaml yaml, final Object json) {
+		if(yaml == null) throw new NullPointerException("yaml argument is required!");
+		if(json == null) throw new NullPointerException("json argument is required!");
+		return yaml.dump(json);
 	}
-	public Map<String, Object> convertMap(final String str) {
+	public static Map<String, Object> convertMap(final Yaml yaml, final String data) throws PacketException {
+		if(yaml == null)        throw new NullPointerException("yaml argument is required!");
+		if(utils.isEmpty(data)) throw new NullPointerException("data argument is required!");
+		final Object obj;
+		try {
+			obj = yaml.load(data);
+		} catch (Exception e) {
+xLog.getRoot().warning("Failed to parse packet json!");
+			throw new PacketException(e);
+		}
 		return utilsObject.castMap(
 				String.class,
 				Object.class,
-				this.getYaml()
-					.load(str)
+				obj
 		);
 	}
-	public List<Object> convertList(final String str) {
+	public static List<Object> convertList(final Yaml yaml, final String data) throws PacketException {
+		if(yaml == null)        throw new NullPointerException("yaml argument is required!");
+		if(utils.isEmpty(data)) throw new NullPointerException("data argument is required!");
+		final Object obj;
+		try {
+			obj = yaml.load(data);
+		} catch (Exception e) {
+xLog.getRoot().warning("Failed to parse packet json!");
+			throw new PacketException(e);
+		}
 		return utilsObject.castList(
 				Object.class,
-				this.getYaml()
-					.load(str)
+				obj
 		);
 	}
 
