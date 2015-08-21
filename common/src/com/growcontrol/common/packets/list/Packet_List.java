e113d861929daa7255315a7019c4dc1336613a88
lorenzop <lorenzo@poixson.com>
2015-08-21 17:56:48 -0400

    added Packet_List class

---
+++ common/src/com/growcontrol/common/packets/list/Packet_List.java
@@ -0,0 +1,55 @@
+package com.growcontrol.common.packets.list;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.growcontrol.common.packets.Packet;
+import com.growcontrol.common.packets.PacketDirection;
+import com.growcontrol.common.packets.PacketProperties;
+
+
+/*
+ * Description: 
+ * Direction: client to server
+ * JSON:
+ * {
+ *     packet: list,
+ *     what: <plugins|zones|..>
+ * }
+ */
+@PacketProperties(
+		name      = "list",
+		stateful  = false,
+		async     = false,
+		direction = PacketDirection.CLIENT_TO_SERVER
+)
+public class Packet_List extends Packet {
+
+
+
+	@Override
+	public Object generate() {
+		final Map<String, String> map = new HashMap<String, String>();
+map.put("list plugins", "ArduinoGC");
+		return map;
+	}
+
+
+
+	@Override
+	public void handle(final String name, final Map<String, Object> json) {
+System.out.println();
+System.out.println();
+System.out.println("GOT PACKET! LIST");
+System.out.println(name);
+System.out.println();
+System.out.println();
+	}
+
+
+
+
+
+
+
+}
