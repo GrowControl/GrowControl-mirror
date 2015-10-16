b1c085c49be05df20bae17b20e258dbd16527b31
Lop <lop@lorenzo.lophome.com>
2015-10-15 20:11:16 -0400

    separate register() functions in apiPlugin classes, rather than a catch-all register()

--- api/src/com/growcontrol/api/clientapi/plugins/apiClientPlugin.java
+++ api/src/com/growcontrol/api/clientapi/plugins/apiClientPlugin.java
@@ -9,22 +9,6 @@ public abstract class apiClientPlugin extends xJavaPlugin {
 
 
 	@Override
-	public void register(final xEventListener listener) {
-		if(listener == null) throw new NullPointerException("Cannot register null listener!");
-//		// command listener
-//		if(listener instanceof xCommandListener) {
-//			gcClientVars.commands()
-//				.register(listener);
-		// unknown
-//		} else {
-			throw new RuntimeException("Cannot register unknown listener type: "
-					+listener.getClass().getName());
-//		}
-	}
-
-
-
-	@Override
 	public void unregister(final Class<? extends xEventListener> listenerClass) {
 		if(listenerClass == null) throw new NullPointerException("Cannot unregister null listener!");
 //		// command listener
--- api/src/com/growcontrol/api/serverapi/plugins/apiServerPlugin.java
+++ api/src/com/growcontrol/api/serverapi/plugins/apiServerPlugin.java
@@ -3,7 +3,7 @@ package com.growcontrol.api.serverapi.plugins;
 import com.growcontrol.common.meta.MetaAddress;
 import com.growcontrol.common.meta.MetaListener;
 import com.growcontrol.common.meta.MetaRouter;
-import com.growcontrol.server.gcServerVars;
+import com.poixson.commonapp.app.xApp;
 import com.poixson.commonapp.plugin.xJavaPlugin;
 import com.poixson.commonjava.xEvents.xEventListener;
 import com.poixson.commonjava.xLogger.xLog;
@@ -14,38 +14,33 @@ public abstract class apiServerPlugin extends xJavaPlugin {
 
 
 
-	@Override
-	public void register(final xEventListener listener) {
-		if(listener == null) throw new NullPointerException("Cannot register null listener!");
-		// command listener
-		if(listener instanceof xCommandListener) {
-			gcServerVars.commands()
-				.register(listener);
-		// unknown
-		} else {
-			throw new RuntimeException("Cannot register unknown listener type: "
-					+listener.getClass().getName());
-		}
-	}
+	// register meta listener
 	public void register(final MetaAddress address, final MetaListener listener) {
-		MetaRouter.get().register(
+		if(address  == null) throw new NullPointerException("Cannot register null meta address!");
+		if(listener == null) throw new NullPointerException("Cannot register null meta listener!");
+		final MetaRouter router = MetaRouter.get();
+		router.register(
 			address,
 			listener
 		);
 	}
-
-
-
+	// register command handler
+	public void register(final xCommandListener listener) {
+		if(listener == null) throw new NullPointerException("Cannot register null command listener!");
+		xApp.get().getCommandsHandler()
+			.register(listener);
+	}
+	// unregister listener type
 	@Override
 	public void unregister(final Class<? extends xEventListener> listenerClass) {
 		if(listenerClass == null) throw new NullPointerException("Cannot unregister null listener!");
 		// command listener
 		if(xCommandListener.class.isInstance(listenerClass)) {
-			gcServerVars.commands()
+			xApp.get().getCommandsHandler()
 				.unregisterType(listenerClass);
 		// unknown
 		} else {
-xLog.getRoot().warning("Unknown listener type, cannot unregister.");
+			xLog.getRoot().warning("Unknown listener type, cannot unregister.");
 //			throw new RuntimeException("Cannot register unknown listener type: "
 //					+listenerClass.getName());
 		}
