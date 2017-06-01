bb75e89cb383c537379f2066a113a05e7df29657
lorenzop <lorenzo@poixson.com>
2017-06-01 17:00:05 -0400

    cleaned up and finished MainBootstrap class for loading client apps

--- client/src/com/growcontrol/client/MainBootstrap.java
+++ client/src/com/growcontrol/client/MainBootstrap.java
@@ -1,142 +1,127 @@
 package com.growcontrol.client;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-
 import com.growcontrol.server.gcServer;
 import com.growcontrol.server.gcServerVars;
+import com.growcontrol.server.gcServerVars.APP_MODE;
+import com.poixson.utils.Failure;
 import com.poixson.utils.Keeper;
 import com.poixson.utils.ShellArgsTool;
 import com.poixson.utils.ThreadUtils;
-import com.poixson.utils.Utils;
 import com.poixson.utils.xVars;
 import com.poixson.utils.xLogger.xLog;
 
 
-public class Main {
+//TODO: https://docs.oracle.com/cd/E19830-01/819-4721/6n6rrfqlv/index.html
+//TODO: http://blog.markturansky.com/archives/21
+public abstract class MainBootstrap {
+
+	private static Keeper keeper;
 
 
 
-	public static void main(final String[] argsArray) {
-		@SuppressWarnings("unused")
-		final Keeper keeper = Keeper.get();
-		Utils.InitAll();
-		// process shell arguments
-		final ShellArgsTool argsTool = ShellArgsTool.init(argsArray);
-		// flags
-		if (argsTool.getFlagBool(false, "-d", "--debug")) {
-			xVars.debug(true);
-		}
-		boolean hasStarted = false;
-		// client with internal server
-		if (argsTool.getFlagBool(false, "-I", "--internal")) {
-			gcServerVars.setAppMode(
-				gcServerVars.APP_MODE.INTERNAL
-			);
-//TODO: internal mode
-			final gcClient client = getClient();
-			client.Start();
-			hasStarted = true;
-		} else {
-			boolean startServer = false;
-			boolean startClient = false;
-			if (argsTool.getFlagBool(false, "-S", "--server")) {
-				startServer = true;
-			}
-			if (argsTool.getFlagBool(false, "-C", "--client")) {
-				startClient = true;
-			}
-			// start server and client
-			if (startServer && startClient) {
-				gcServerVars.setAppMode(
-					gcServerVars.APP_MODE.SERVER_CLIENT
-				);
-			}
-			// start server
-			if (startServer) {
-				gcServerVars.setAppMode(
-					gcServerVars.APP_MODE.SERVER_ONLY
-				);
-				final gcServer server = new gcServer();
-				server.Start();
-				hasStarted = true;
-			}
-			// start client
-			if (startClient) {
-				gcServerVars.setAppMode(
-					gcServerVars.APP_MODE.CLIENT_ONLY
-				);
-				final gcClient client = getClient();
-				client.Start();
-				hasStarted = true;
-			}
-		}
-		// default start mode
-		if ( ! hasStarted) {
-//TODO: detect gui
-//			hasStarted = true;
-//ThreadUtils.displayStillRunning();
-		}
-
-System.out.println();
-System.out.println(
-	xVars.debug()
-	? "YES"
-	: "NO"
-);
-System.out.println();
-
-		// done
-		xLog.getRoot()
-			.fine("Initial thread returning..");
-		ThreadUtils.Sleep(150L);
+	protected MainBootstrap() {
+		if (keeper != null) throw new RuntimeException("MainBootstrap already loaded?");
+		keeper = Keeper.get();
 	}
 
 
 
-	private static gcClient getClient() {
-		final ClientProps props = new ClientProps(Main.class);
-		// get app class
-		final Class<?> clss;
-		try {
-			clss = Class.forName(props.mainClass);
-		} catch (ClassNotFoundException e) {
-//TODO:
-			e.printStackTrace();
-			return null;
+	protected abstract gcClient newClient();
+
+
+
+//TODO: detect gui/shell
+	public void doMain(final String[] argsArray) {
+		// process shell arguments
+		final ShellArgsTool argsTool = ShellArgsTool.init(argsArray);
+		// app mode flags
+		{
+			final boolean internalMode =
+				argsTool.getFlagBool(false, "-I", "--internal");
+			final boolean serverMode =
+				argsTool.getFlagBool(false, "-S", "--server");
+			final boolean clientMode =
+				argsTool.getFlagBool(false, "-C", "--client");
+			// internal mode
+			if (internalMode) {
+				gcServerVars
+					.setAppMode(APP_MODE.INTERNAL);
+			} else
+			// client and local server
+			if (serverMode && clientMode) {
+				gcServerVars
+					.setAppMode(APP_MODE.SERVER_CLIENT);
+			} else
+			// server only
+			if (serverMode) {
+				gcServerVars
+					.setAppMode(APP_MODE.SERVER_ONLY);
+			} else
+			// client only
+			if (clientMode) {
+				gcServerVars
+					.setAppMode(APP_MODE.CLIENT_ONLY);
+			}
 		}
-		// get app constructor
-		final Constructor<?> construct;
-		try {
-			construct = clss.getConstructor();
-		} catch (NoSuchMethodException e) {
+		// start app mode
+		{
+			final APP_MODE appMode = gcServerVars.getAppMode();
+			// default
+			if (appMode == null) {
 //TODO:
-			e.printStackTrace();
-			return null;
-		} catch (SecurityException e) {
-			e.printStackTrace();
-			return null;
-		}
-		// new instance of app class
-		final gcClient client;
-		try {
-			final Object obj = construct.newInstance();
-			client = (gcClient) obj;
-		} catch (InstantiationException e) {
+throw new RuntimeException("UNFINISHED MODE");
+			} else {
+				gcServer server = null;
+				gcClient client = null;
+				switch (appMode) {
+				case INTERNAL:
 //TODO:
-			e.printStackTrace();
-			return null;
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-			return null;
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-			return null;
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-			return null;
+//					final gcClient client = newClient();
+//					client.Start();
+throw new RuntimeException("UNFINISHED MODE");
+				case SERVER_CLIENT:
+					try {
+						server = new gcServer();
+						server.Start();
+					} catch (Exception e) {
+						Failure.fail(e);
+					}
+					ThreadUtils.Sleep(100L);
+					try {
+						client = this.newClient();
+						client.Start();
+					} catch (Exception e) {
+						Failure.fail(e);
+					}
+					break;
+				case SERVER_ONLY:
+					try {
+						server = new gcServer();
+						server.Start();
+					} catch (Exception e) {
+						Failure.fail(e);
+					}
+					break;
+				case CLIENT_ONLY:
+					try {
+						client = this.newClient();
+						client.Start();
+					} catch (Exception e) {
+						Failure.fail(e);
+					}
+					break;
+				default:
+					//TODO:
+					throw new RuntimeException("UNFINISHED MODE");
+				}
+			}
 		}
-		return client;
+		// finished starting
+		if (xVars.debug()) {
+			xLog.getRoot()
+				.fine("Initial thread returning..");
+		}
+		ThreadUtils.Sleep(100L);
 	}
 
 
