1aaaefecd6a308a8ef9ee4daf9a9762e690bce42
lorenzop <lorenzo@poixson.com>
2020-05-28 20:19:54 -0400

    added empty Main class to studio and lab projects

---
+++ lab/src/com/growcontrol/client/lab/Main.java
@@ -0,0 +1,13 @@
+package com.growcontrol.client.lab;
+
+
+public class Main {
+
+
+
+	public static void main(final String[] args) {
+	}
+
+
+
+}
---
+++ studio/src/com/growcontrol/client/studio/Main.java
@@ -0,0 +1,13 @@
+package com.growcontrol.client.studio;
+
+
+public class Main {
+
+
+
+	public static void main(final String[] args) {
+	}
+
+
+
+}
