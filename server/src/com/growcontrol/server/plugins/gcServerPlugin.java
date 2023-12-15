127f5e64b8c74069067e4845c4fc353de6eeb81c
lorenzop <lorenzo@poixson.com>
2023-12-14 22:39:30 -0500

    cleaned up gcServerPlugin classes

--- server/src/com/growcontrol/server/plugins/gcServerPlugin.java
+++ server/src/com/growcontrol/server/plugins/gcServerPlugin.java
@@ -1,55 +1,54 @@
-/*
 package com.growcontrol.server.plugins;
 
+import com.growcontrol.server.gcServer;
+import com.growcontrol.server.configs.gcServerConfig;
 import com.poixson.plugins.xJavaPlugin;
+import com.poixson.plugins.xPluginManager;
+import com.poixson.plugins.xPluginYML;
+import com.poixson.tools.config.xConfig;
 
 
 public abstract class gcServerPlugin extends xJavaPlugin {
 
+	public static final String KEY_CLASS_MAIN = "Server Main";
+
+	protected final gcServer server;
 
 
-	public gcServerPlugin() {
-		super();
+
+	public gcServerPlugin(final gcServer server,
+			final xPluginManager<gcServerPlugin> manager, final xPluginYML yml) {
+		super(manager, yml);
+		this.server = server;
 	}
 
 
 
-//TODO:
-/ *
+	public gcServer getServer() {
+		return this.server;
+	}
+	public xConfig getPluginConfig() {
+		final gcServer server = this.getServer();
+		final gcServerConfig config = server.getConfig();
+		return config.getConfig(this.getPluginNameSafe());
+	}
+
+
+
+//TODO
+/*
 	// register meta listener
 	public void register(final MetaAddress address, final MetaListener listener) {
-		if(address  == null) throw new IllegalArgumentException("Cannot register null meta address!");
-		if(listener == null) throw new IllegalArgumentException("Cannot register null meta listener!");
+		if (address  == null) throw new RequiredArgumentException("address");
+		if (listener == null) throw new RequiredArgumentException("listener");
 		final MetaRouter router = MetaRouter.get();
 		router.register(
 			address,
 			listener
 		);
 	}
-	// register command handler
-	public void register(final xCommandListener listener) {
-		if(listener == null) throw new IllegalArgumentException("Cannot register null command listener!");
-		xApp.get().getCommandsHandler()
-			.register(listener);
-	}
-	// unregister listener type
-	@Override
-	public void unregister(final Class<? extends xEventListener> listenerClass) {
-		if(listenerClass == null) throw new IllegalArgumentException("Cannot unregister null listener!");
-		// command listener
-		if(xCommandListener.class.isInstance(listenerClass)) {
-			xApp.get().getCommandsHandler()
-				.unregisterType(listenerClass);
-		// unknown
-		} else {
-			xLog.getRoot().warning("Unknown listener type, cannot unregister!");
-//			throw new RuntimeException("Cannot register unknown listener type: "
-//					+listenerClass.getName());
-		}
-	}
-* /
+*/
 
 
 
 }
-*/
